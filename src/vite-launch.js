a11fce36edf17ddd6a383c777aefcde2f256b8e1
gongrui.wh <527389031@qq.com>
2020-07-16 10:03:21 +0800

    兼容window

--- src/vite-launch.js
+++ src/vite-launch.js
@@ -1,81 +1,87 @@
 /**
  * 启动vite
  */
-const vscode = require('vscode');
-const exec = require('child_process').exec;
-const vite = require('vite')
-
+const vscode = require("vscode");
+// const exec = require("child_process").exec;
+const vite = require("vite");
+const open = require("open");
 
 module.exports = function (context) {
   let server = null;
-  context.subscriptions.push(vscode.commands.registerCommand('extension.vite.viteLaunch', function (uri) {
-    // 工程目录
-    const workspaceFolder = vscode.workspace.workspaceFolders[0]
-    const workspacePath = workspaceFolder.uri.path
-    let relativePath = '/index.html'
-    if (uri) {
-      relativePath = uri.path.substring(workspacePath.length)
-    }
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.vite.viteLaunch", function (
+      uri
+    ) {
+      // 工程目录
+      const workspaceFolder = vscode.workspace.workspaceFolders[0];
+      const workspacePath = workspaceFolder.uri.fsPath;
+      // const workspacePath = "D:\\llbt\\ant-design-pro-vue";
+      let relativePath = "/index.html";
+      if (uri) {
+        relativePath = uri.fsPath.substring(workspacePath.length);
+      }
+      const options = {
+        root: workspacePath,
+        port: 3000,
+      };
 
-    const options = {
-      root: workspacePath,
-      port: 3000
-    }
+      if (!server || !server.listening) {
+        server = runServe(options);
+      }
+      statusBarItem.text = `$(debug-pause) vite: ${options.port}`;
+      statusBarItem.command = "extension.vite.viteClose";
 
-    if (!server || !server.listening) {
-      server = runServe(options)
-    }
-
-    statusBarItem.text = `$(debug-pause) vite: ${options.port}`
-    statusBarItem.command = 'extension.vite.viteClose'
-
-    exec(`open http://localhost:${options.port}${relativePath}`)
-  }))
-
-
-  context.subscriptions.push(vscode.commands.registerCommand('extension.vite.viteClose', function () {
-    statusBarItem.text = `closing`
-    statusBarItem.command = null
-    server && server.close(() => {
-      vscode.window.showInformationMessage('vite is closed')
-      statusBarItem.text = `$(debug-start) vite  `
-      statusBarItem.command = 'extension.vite.viteLaunch'
+      open(`http://localhost:${options.port}${relativePath}`);
     })
-  }))
+  );
 
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.vite.viteClose", function () {
+      statusBarItem.text = `closing`;
+      statusBarItem.command = null;
+      server &&
+        server.close(() => {
+          vscode.window.showInformationMessage("vite is closed");
+          statusBarItem.text = `$(debug-start) vite  `;
+          statusBarItem.command = "extension.vite.viteLaunch";
+        });
+    })
+  );
 
-  const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100)
-  statusBarItem.text = `$(debug-start) vite`
-  statusBarItem.command = 'extension.vite.viteLaunch'
-  context.subscriptions.push(statusBarItem)
+  const statusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    100
+  );
+  statusBarItem.text = `$(debug-start) vite`;
+  statusBarItem.command = "extension.vite.viteLaunch";
+  context.subscriptions.push(statusBarItem);
 
   statusBarItem.show();
-}
+};
 
 function runServe(options) {
-  const server = vite.createServer(options)
+  const server = vite.createServer(options);
 
-  let port = options.port || 3000
-  let hostname = options.hostname || 'localhost'
-  const protocol = options.https ? 'https' : 'http'
+  let port = options.port || 3000;
+  let hostname = options.hostname || "localhost";
+  const protocol = options.https ? "https" : "http";
 
-  server.on('error', (e) => {
+  server.on("error", (e) => {
     // @ts-ignore
-    if (e.code === 'EADDRINUSE') {
-      console.log(`Port ${port} is in use, trying another one...`)
+    if (e.code === "EADDRINUSE") {
+      console.log(`Port ${port} is in use, trying another one...`);
       setTimeout(() => {
-        server.close()
-        server.listen(++port)
-      }, 100)
+        server.close();
+        server.listen(++port);
+      }, 100);
     } else {
-      console.error(e)
+      console.error(e);
     }
-  })
+  });
 
   server.listen(port, () => {
-    console.log(`Dev server running at: ${port}`)
-  })
+    console.log(`Dev server running at: ${port}`);
+  });
 
-  return server
+  return server;
 }
-
